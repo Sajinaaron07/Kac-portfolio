9723f5332fb5b2d9bf00ea91d6bd6e86eb46171a
Sajinaaron07 <86417527+Sajinaaron07@users.noreply.github.com>
2022-08-31 14:55:47 +0530

    Update base.py

--- base.py
+++ base.py
@@ -11,4 +11,4 @@ def hello_world():
 
 if __name__ == "__main__":
     port = int(os.getenv('PORT'))
-    app.run(port = int(os.getenv('PORT'))s)
+    app.run(Debug = False,port = int(os.getenv('PORT'))s)
